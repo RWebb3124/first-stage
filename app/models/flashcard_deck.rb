d95dc8d4e8c6a5c7fab465e372c920e51961f5d9
Luke Kucinski <lukek22b@gmail.com>
2024-07-15 18:05:58 +0200

    setup

--- app/models/flashcard_deck.rb
+++ app/models/flashcard_deck.rb
@@ -1,3 +1,4 @@
 class FlashcardDeck < ApplicationRecord
   belongs_to :user
+  has_many :flashcards
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :bookings_as_interviewee, class_name: ‘booking’, foreign_key: :interviewee_id
+  has_many :bookings_as_interviewer, class_name: ‘booking’, foreign_key: :interviewer_id
+  has_many :reviews, through: :bookings
+  has_many :user_tags
+  has_many :tags, through: :user_tags
+  has_many :notifications
+  has_many :chatrooms
+  has_many :messages, through: :chatrooms
+  has_many :flashcard_decks
+  has_many :flashcards, through: :flashcard_decks
 end
