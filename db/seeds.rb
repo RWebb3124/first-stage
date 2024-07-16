3e9da0fd1fa68099879f5fe29d3ff6ef3d34367a
Ryan Webb <rwebb3124@gmail.com>
2024-07-16 15:27:20 +0100

    seed finished and controller added

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,46 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+UserTag.destroy_all
+User.destroy_all
+
+ruby_on_rails = Tag.create!({ name: "Ruby on Rails", icon_url: "#" })
+javascript = Tag.create!({ name: "Javascript", icon_url: "#" })
+css = Tag.create!({ name: "CSS", icon_url: "#" })
+
+5.times do
+  User.create!(
+    {
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      email: Faker::Internet.email,
+      password: "Password1234",
+      username: Faker::Internet.username,
+      address: Faker::Address.city
+    }
+  )
+end
+
+5.times do
+  interviewer = User.new(
+    {
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      email: Faker::Internet.email,
+      password: "Password1234",
+      username: Faker::Internet.username,
+      address: Faker::Address.city,
+      interviewer: true,
+      years_experience: 6,
+      headline: "This is my tagline",
+      about: "This is a story all about how my life got flipped on upside down"
+    }
+  )
+  interviewer.save!
+
+  UserTag.create!({ user: interviewer, tag: ruby_on_rails })
+  UserTag.create!({ user: interviewer, tag: javascript })
+  UserTag.create!({ user: interviewer, tag: css })
+
+end
