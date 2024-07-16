8ad8a524f61e0453b04d0d905720d1e68a282bfe
Petya <pettya.gg@gmail.com>
2024-07-16 17:06:01 +0200

    user new and edit form updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,23 @@ class UsersController < ApplicationController
   def index
     @users = User.where(interviewer: true)
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, notice: "User created successfully!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:emial, :password, :password_confirmation, :first_name, :last_name, :years_experience, :address, :username)
+  end
 end
