eaada8759e0e5853e953647a747843ae743f2fcb
Yannick Bierens <yannick@urbanjournalist.com>
2024-07-17 16:27:29 +0200

    save data for signup fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,23 +8,4 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to root_path, notice: "User created successfully!"
-    else
-      render :new
-    end
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:emial, :password, :password_confirmation, :first_name, :last_name, :years_experience, :address, :username)
-  end
 end
